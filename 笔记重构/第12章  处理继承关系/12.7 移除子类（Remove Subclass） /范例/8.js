e39e8308ff53371733e6eb7ae2a25c584585ded1
王帅 <wangshuaishuai@yidiansongli.com>
2021-07-01 14:22:20 +0800

    12

--- 笔记重构/第12章  处理继承关系/12.7 移除子类（Remove Subclass） /范例/8.js
+++ 笔记重构/第12章  处理继承关系/12.7 移除子类（Remove Subclass） /范例/8.js
@@ -5,18 +5,32 @@ class Person {
     constructor(name) {
         this._name = name;
     }
-    get name() { return this._name; }
-    get genderCode() { return "X"; }
-    get isMale() { return this instanceof Male; }
+
+    get name() {
+        return this._name;
+    }
+
+    get genderCode() {
+        return "X";
+    }
+
+    get isMale() {
+        return this instanceof Male;
+    }
 }
 
 
 // snip
 class Male extends Person {
-    get genderCode() { return "M"; }
+    get genderCode() {
+        return "M";
+    }
 }
+
 class Female extends Person {
-    get genderCode() { return "F"; }
+    get genderCode() {
+        return "F";
+    }
 }
 
 function createPerson(aRecord) {
@@ -35,16 +49,15 @@ function loadFromInput(data) {
 }
 
 
-
 // 客户端...
-const numberOfMales = people.filter(p => p.isMale ).length;
+const numberOfMales = people.filter(p => p.isMale).length;
 
 
 /**
-    重构到这一步，所有与子类相关的知识都已经安全地包装在超类和工厂函数中。（对于“超类引用子类”这种情况，
+ 重构到这一步，所有与子类相关的知识都已经安全地包装在超类和工厂函数中。（对于“超类引用子类”这种情况，
  通常我会很警惕，不过这段代码用不了一杯茶的工夫就会被干掉，所以也不用太担心。）
 
-    现在，添加一个字段来表示子类之间的差异。既然有来自别处的一个类型代码，直接用它也无妨。
+ 现在，添加一个字段来表示子类之间的差异。既然有来自别处的一个类型代码，直接用它也无妨。
  */
 
 
